3d54610bc90e33ef1ea06a6f2b8aeb2ead84b168
verney <username@example.com>
2024-06-19 21:41:04 -0400

    Limiting the Ship's Range

--- ship.py
+++ ship.py
@@ -36,9 +36,12 @@ class Ship():
     def update(self):
         """Update the ship's position base on the movement flag."""
         # Update the ship's x value, not the rect.
-        if self.moving_right:
+        # Check the position of the ship before changing the value of self.x.
+
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         # Update rect object from self.x.
