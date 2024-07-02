26f5a8d93d574574e242e898d1e3617819920695
Verney <verney.sotodezma@gmail.com>
2024-07-01 21:12:50 -0400

    Create Settings for Fleet Direction

--- settings.py
+++ settings.py
@@ -26,4 +26,9 @@ class Settings:
 
         #Alien settings: alien's speed:
         self.alien_speed=1.0
+        # Control the speed of the fleet drops down each time an alien reaches
+        # either edge.
+        self.fleet_drop_speed=10
+        # fleet_direction of 1 represents right; -1 represents left
+        self.fleet_direction=1
 
